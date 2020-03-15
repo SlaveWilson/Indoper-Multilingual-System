b417dcb38a76c42b8410b2399ea693a0d9fb80f1
TNTommy <tommynhm@gmail.com>
2020-03-15 16:06:41 +0800

    Init commit

---
+++ Assets/Scripts/TextLocaliserUI.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextLocaliserUI : MonoBehaviour
+{
+    TextMeshProUGUI textField;
+
+    public LocalisedString localisedString;
+
+    private void Start()
+    {
+        textField = GetComponent<TextMeshProUGUI>();
+        string value = localisedString.value;
+        textField.text = value;
+    }
+}
