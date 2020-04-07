a4f035c915eabba14fcc55d261484d0b98c9a2fe
TNTommy <tommynhm@gmail.com>
2020-04-08 02:25:15 +0800

    Revert back to origin

---
+++ Assets/Prefabs/Indoper/Multilingual/Scripts/TextLocaliserUI.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextLocaliserUI : MonoBehaviour
+{
+    TextMeshProUGUI textField;
+
+    public LocalisedString localisedString;
+
+    private void Start()
+    {
+        textField = GetComponent<TextMeshProUGUI>();
+        string value = localisedString.value;
+        textField.text = value;
+    }
+}
